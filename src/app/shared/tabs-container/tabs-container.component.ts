905df81406e6fd6c30ff28ba4099ae6c3100f66b
Rafael Lima <rafaa99@gmail.com>
2022-06-09 10:47:27 -0300

    add active tab property

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -12,11 +12,18 @@ import { TabComponent } from '../tab/tab.component';
   styleUrls: ['./tabs-container.component.scss'],
 })
 export class TabsContainerComponent implements AfterContentInit {
-  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = {};
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> =
+    new QueryList();
 
   constructor() {}
 
   ngAfterContentInit(): void {
-    console.log(this.tabs);
+    const activeTabs = this.tabs.filter((tab) => tab.active);
+    if (!activeTabs || activeTabs.length === 0)
+      this.selectTab(this.tabs!.first);
+  }
+  selectTab(tab: TabComponent) {
+    this.tabs.forEach((tab) => (tab.active = false));
+    tab.active = true;
   }
 }
