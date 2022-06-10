72af8b636f0be1ca63f96ecd59c735d081afdf8d
Rafael Lima <rafaa99@gmail.com>
2022-06-10 18:44:31 -0300

    refactor Input Component validators

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
+  @Input() control: FormControl = new FormControl();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
