05ce86da4e4f3e723644978bac6ff451fbef59f8
Rafael Lima <rafaa99@gmail.com>
2022-06-15 07:47:40 -0300

    add userCollection type

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+  private userCollection: AngularFirestoreCollection<IUser>;
+
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+    this.userCollection = db.collection('users');
+  }
 
   public async createUser(userData: IUser) {
+    if (!userData.password) {
+      throw new Error('Password is required');
+    }
     const userCredentials = await this.auth.createUserWithEmailAndPassword(
       userData.email,
       userData.password
     );
-    await this.db.collection('users').add({
+    await this.userCollection.add({
       name: userData.name,
       email: userData.email,
       age: userData.age,
