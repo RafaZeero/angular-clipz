952c8f061551953fa08f58027f3ea94f7ac89ce4
Rafael Lima <rafaa99@gmail.com>
2022-06-15 10:03:25 -0300

    add login logic

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -7,19 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   showAlert = false;
-  alertMsg = 'Please wait! Your account is being created.';
+  alertMsg = 'Logging in, please wait...';
   alertColor = 'blue';
+
+  inSubmission = false;
 
   credentials = {
     email: '',
     password: '',
   };
-  constructor() {}
+  constructor(private auth: AngularFireAuth) {}
 
   ngOnInit(): void {}
 
-  login() {
+  async login() {
     this.showAlert = true;
+    this.alertMsg = 'Logging in, please wait...';
     this.alertColor = 'blue';
+    this.inSubmission = true;
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (err) {
+      console.log(err);
+      this.alertMsg = 'An unexpected error occurred. Please try again later';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+    this.alertMsg = 'Login successfully';
+    this.alertColor = 'blue';
+    this.inSubmission = false;
   }
 }
