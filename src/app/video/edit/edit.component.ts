dde92c5136f7493d184125b0b772f743c9cc5d21
Rafael Lima <rafaa99@gmail.com>
2022-06-25 11:54:00 -0300

    add edit form logic

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import IClip from 'src/app/models/clip.model';
+import { ClipService } from 'src/app/services/clip.service';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,13 +9,60 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss'],
 })
-export class EditComponent implements OnInit, OnDestroy {
-  constructor(private modal: ModalService) {}
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
+  @Input() activeClip: IClip | null = null;
+  inSubmission = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait! Updating clip.';
+
+  clipID = new FormControl('', { nonNullable: true });
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+
+  // form group to edit a clip
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID,
+  });
+
+  constructor(private modal: ModalService, private clipService: ClipService) {}
 
   ngOnInit(): void {
     this.modal.register('editClip');
   }
   ngOnDestroy(): void {
     this.modal.unregister('editClip');
+  }
+  ngOnChanges(): void {
+    if (!this.activeClip) return;
+
+    this.clipID.setValue(this.activeClip.docID!);
+    this.title.setValue(this.activeClip.title);
+  }
+
+  async submit() {
+    this.inSubmission = true;
+
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! Updating clip.';
+
+    try {
+      await this.clipService.updateClip(this.clipID.value, this.title.value);
+    } catch (err) {
+      this.inSubmission = false;
+
+      this.alertColor = 'red';
+      this.alertMsg = 'Something went wrong! Please try again later.';
+
+      return;
+    }
+    this.inSubmission = false;
+
+    this.alertColor = 'green';
+    this.alertMsg = 'Success!';
   }
 }
