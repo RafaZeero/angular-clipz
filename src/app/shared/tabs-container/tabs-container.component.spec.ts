604916a48049c7c0e2e2621532ae6df04fd65605
Rafael Lima <rafaa99@gmail.com>
2022-08-16 22:32:16 -0300

    test tab-container component

---
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TabsContainerComponent } from './tabs-container.component';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  template: `
+    <app-tabs-container>
+      <app-tab tabTitle="Tab 1">Tab 1</app-tab>
+      <app-tab tabTitle="Tab 2">Tab 2</app-tab>
+    </app-tabs-container>
+  `,
+})
+class TestHostComponent {}
+
+describe('TabsContainerComponent', () => {
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TabsContainerComponent, TabComponent, TestHostComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestHostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
